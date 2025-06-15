5a2ef8da739e9d17062ebeb878e04c7f518a1b1d
Kim Sanghae <pelikan@pusan.ac.kr>
2025-06-15 18:40:27 +0900

    feat: validation 관련 annotation 추가
    - Email 어노테이션
    - 비밀번호 길이 어노테이션 추가

--- src/main/java/com/example/demo/src/user/model/PostUserReq.java
+++ src/main/java/com/example/demo/src/user/model/PostUserReq.java
@@ -1,28 +1,49 @@
 package com.example.demo.src.user.model;
 
 import com.example.demo.src.user.entity.User;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+import org.apache.commons.lang3.builder.ToStringExclude;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 public class PostUserReq {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    @Email(message = "이메일 형식이 올바르지 않습니다.")
+    @Size(max = 100, message = "이메일은 100자 이하여야 합니다.")
     private String email;
+
+    @ToStringExclude
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8-20자 사이여야 합니다.")
+    @Pattern(
+        regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+        message = "비밀번호는 영문, 숫자, 특수문자를 포함해야 합니다.")
     private String password;
+
+    @NotBlank(message = "이름은 필수입니다.")
+    @Size(min=2,max=20,message = "이름은 2-20자 이상이어야 합니다.")
     private String name;
 
     private boolean isOAuth;
 
     public User toEntity() {
         return User.builder()
-                .email(this.email)
-                .password(this.password)
-                .name(this.name)
-                .isOAuth(this.isOAuth)
-                .build();
+            .email(this.email)
+            .password(this.password)
+            .name(this.name)
+            .isOAuth(this.isOAuth)
+            .build();
     }
 }
